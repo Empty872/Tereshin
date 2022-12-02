e022f1082d187f6064ca22c2a4498d9fc39ef067
Vladimir <vladimir.tereshin@urfu.me>
2022-12-03 01:19:35 +0500

    изменил choose_way на choose_method

--- 2.2.2.py
+++ 2.2.2.py
@@ -1,11 +1,11 @@
 import Task_2_1_3
 import Task_4_3
 
-choose_way = input("Введите \"таблица\", если вам нужна таблица, введите \"pdf\", если вам нужен pdf \n")
+choose_method = input("Введите \"таблица\", если вам нужна таблица, введите \"pdf\", если вам нужен pdf \n")
 
-if choose_way == "таблица":
+if choose_method == "таблица":
     Task_4_3.run_program()
-elif choose_way == "pdf":
+elif choose_method == "pdf":
     Task_2_1_3.run_program()
 else:
     print("введены неправильные данные")
